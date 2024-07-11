638971a0490dad71bdd05d6084649d1b3ae69d0f
Mariam Kavtaradze <mariakavt@gmail.com>
2024-07-11 13:10:22 +0400

    created Client and Server class, wrote Client logic

---
+++ src/Client.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try{
+            Socket socket = new Socket("localhost", 9999 );
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            Scanner scanner = new Scanner(System.in);
+
+            new Thread(() -> {
+                try {
+                    Scanner inScanner = new Scanner(inputStream);
+                    while (inScanner.hasNextLine()) {
+                        System.out.println(inScanner.nextLine());
+                    }
+                } catch (Exception e) {
+                    System.err.println("Error reading from server: " + e.getMessage());
+                }
+            }).start();
+
+            // Write to server
+            while (true) {
+                String msgToSend = scanner.nextLine();
+                outputStream.write((msgToSend + "\n").getBytes());
+                outputStream.flush();
+
+                if (scanner.hasNextLine()) {
+                    System.out.println("Server: " + scanner.nextLine());
+                }
+            }
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
